7c58be6b6427b126556b80e46053619a8b63b314
Robbie Bendick <robbiebendick@gmail.com>
2023-12-04 08:29:46 -0800

    compartmentalize

--- ArenaCoach.lua
+++ ArenaCoach.lua
@@ -1,5 +1,35 @@
 local ArenaCoach, core = ...;
 core.AC = {};
+Config = core.Config;
 AC = core.AC;
+local addon_name = "ArenaCoach";
+local ArenaStats = _G.LibStub("AceAddon-3.0"):NewAddon(addon_name,
+                                                       "AceConsole-3.0",
+                                                       "AceEvent-3.0");
 
+local LibDBIcon = LibStub("LibDBIcon-1.0")
+
+function AC:OnInitialize()
+    self.db = _G.LibStub("AceDB-3.0"):New(addon_name, {
+        profile = {
+            minimapButton = {hide = false},
+        },
+    })
+    self:DrawMinimapIcon()
+end
+
+function AC:DrawMinimapIcon()
+    -- Register minimap icon
+    LibDBIcon:Register(addon_name, {
+        icon = "Interface\\Icons\\Ability_Creature_Cursed_01",
+        OnClick = core.Config.OpenMinimapConfig,
+        OnTooltipShow = function(tt)
+            tt:AddLine(addon_name);
+            tt:AddLine("Click to open options");
+        end,
+        text = addon_name,
+        iconCoords = {0.05, 0.95, 0.05, 0.95},
+    })
+end
+AC:OnInitialize();
 
--- Config.lua
+++ Config.lua
@@ -5,29 +5,29 @@ local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local LibDBIcon = LibStub("LibDBIcon-1.0")
 local addon_name = "ArenaCoach"
 local frame;
+AC = core.AC;
 core.Config = {};
 Config = core.Config;
 Config.filter = {};
 filter = Config.filter;
-local _, player_class = UnitClass("player");
 filter.bracket_filter = "2v2";
 filter.player_comp = "";
 filter.enemy_comp = "";
-local is_player_comp_empty, is_enemy_comp_empty = filter.player_comp == "", filter.enemy_comp;
+local _, player_class = UnitClass("player");
+local addon_name = "ArenaCoach";
 
-function OpenMinimapConfig()
+function Config:OpenMinimapConfig()
     if frame and frame:IsShown() then
         frame:Hide();
     else
         if not frame then
-            frame = CreateCustomConfigFrame();
+            frame = Config:CreateCustomConfigFrame();
         end
         frame:Show();
     end
 end
 
-
-function CreateCustomConfigFrame()
+function Config:CreateCustomConfigFrame()
     local frame_width, frame_height = 900, 420;
     local f = AceGUI:Create("Frame");
 
@@ -45,8 +45,8 @@ function CreateCustomConfigFrame()
     bracket_dropdown:SetWidth(72);
     bracket_dropdown:SetCallback("OnValueChanged", function(_, _, bracket)
         filter.bracket_filter = bracket;
-        UpdatePlayerClassDropdowns();
-        ResetDropdowns();
+        Config:UpdatePlayerClassDropdowns();
+        Config:ResetDropdowns();
     end);
     bracket_dropdown:SetValue("2v2");
     f:AddChild(bracket_dropdown);
@@ -57,8 +57,7 @@ function CreateCustomConfigFrame()
     player_comp_dropdown:SetList(viablePlayerComps);
     player_comp_dropdown:SetCallback("OnValueChanged", function(_, _, comp)
         filter.player_comp = comp;
-        UpdateSummaryAndTipText();
-        UpdateUserHelperText();
+        Config:UpdateSummaryAndTipText();
     end);
     player_comp_dropdown:SetWidth(220);
     f:AddChild(player_comp_dropdown);
@@ -74,13 +73,12 @@ function CreateCustomConfigFrame()
     enemy_comp_dropdown:SetList(core.VIABLE_COMPS[filter.bracket_filter]);
     enemy_comp_dropdown:SetCallback("OnValueChanged", function(_, _, comp)
         filter.enemy_comp = comp;
-        UpdateSummaryAndTipText();
-        UpdateUserHelperText();
+        Config:UpdateSummaryAndTipText();
     end);
     enemy_comp_dropdown:SetWidth(220);
     f:AddChild(enemy_comp_dropdown);
 
-    function UpdatePlayerClassDropdowns(selectedBracket)
+    function Config:UpdatePlayerClassDropdowns()
         local playerClassCombinations = core[player_class.."_COMPS"][filter.bracket_filter];
         local viableEnemyComps = core.VIABLE_COMPS[filter.bracket_filter];
         player_comp_dropdown:SetList(playerClassCombinations);
@@ -93,15 +91,18 @@ function CreateCustomConfigFrame()
     horizontal_group:SetWidth(frame_width - 20);
 
     local summary_text_label = AceGUI:Create("Label");
-    summary_text_label:SetText("|cffff0000Please select your comp and an enemy comp.|r");
+    summary_text_label:SetText("");
     horizontal_group:AddChild(summary_text_label);
+
+    local error_text_label = AceGUI:Create("Label");
+    horizontal_group:AddChild(error_text_label);
 
     local tips_text_label = AceGUI:Create("Label");
 
     horizontal_group:AddChild(tips_text_label);
     f:AddChild(horizontal_group);
 
-    function UpdateSummaryAndTipText()
+    function Config:UpdateSummaryAndTipText()
         if filter.player_comp == "" or filter.enemy_comp == "" then return end
         local summary = core[player_class .. "_STRATS"][filter.bracket_filter][filter.player_comp][filter.enemy_comp].summary or "";
         local tips = core[player_class .. "_STRATS"][filter.bracket_filter][filter.player_comp][filter.enemy_comp].tips or {};
@@ -120,20 +121,14 @@ function CreateCustomConfigFrame()
         tips_text_label:SetText(formattedTips);
         summary_text_label:SetText((summary ~= "" and "|cff33ff99Summary|r\n\n" or coloredSummaryTitle .. "Summary not yet provided.") .. summary);
     end
-    function UpdateUserHelperText()
-        if filter.player_comp == "" then
-            summary_text_label:SetText("|cffff0000Please select your comp.|r");
-        elseif filter.enemy_comp == ""  then
-            summary_text_label:SetText("|cffff0000Please select an enemy comp.|r");
-        end
-    end
-    function ResetDropdowns()
+
+    function Config:ResetDropdowns()
         player_comp_dropdown:SetValue(nil);
         enemy_comp_dropdown:SetValue(nil);
         filter.enemy_comp = "";
         filter.player_comp = "";
-        tips_text_label:SetText("Please select your comp and an enemy comp");
-        summary_text_label:SetText("Please select your comp and an enemy comp");
+        tips_text_label:SetText("");
+        summary_text_label:SetText("");
     end
 
     return f;
@@ -144,16 +139,4 @@ end
 -- register slash command to open options
 SLASH_ARENACOACH1 = "/arenacoach";
 SLASH_ARENACOACH2 = "/ac";
-SlashCmdList["ARENACOACH"] = OpenMinimapConfig;
-
--- register minimap icon
-LibDBIcon:Register(addon_name, {
-    icon = "Interface\\Icons\\Ability_Creature_Cursed_01",
-    OnClick = OpenMinimapConfig,
-    OnTooltipShow = function(tt)
-        tt:AddLine(addon_name);
-        tt:AddLine("Click to open options");
-    end,
-    text = addon_name,
-    iconCoords = {0.05, 0.95, 0.05, 0.95},
-});
+SlashCmdList["ARENACOACH"] = Config.OpenMinimapConfig;
